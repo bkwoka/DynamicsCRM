3d1328bcddd3b7a6d47d9126e4ae6d02a32827b5
Bartek <bkwoka1@gmail.com>
2022-01-26 00:31:42 +0100

    Add project files.

---
+++ StudentPlugin/Constants/Constants.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmPackage.StudentPlugin.Constants
+{
+    internal class Constants
+    {
+        public enum Semester
+        {
+            Smester1 = 821250000,
+            Smester2 = 821250001,
+            Smester3 = 821250002,
+            Smester4 = 821250003,
+            Smester5 = 821250004,
+            Smester6 = 821250005,
+            Smester7 = 821250006,
+            Smester8 = 821250007
+        }
+
+        public enum Cycle
+        {
+            Pcycle = 821250000,
+            Ccycle = 821250001,
+            Both = 821250002
+        }
+    }
+}
